783916ed7ccae536b37f05dea0f876840675df69
Gabriel Reis <gabrielcnr@gmail.com>
2021-12-06 22:33:12 +0000

    day 6 part 2

--- day06.py
+++ day06.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import aoc
 
 
@@ -20,6 +22,29 @@ def part1(data: str, *, days_after: int = 0):
     return len(fishes)
 
 
+def part2(data: str, *, days_after: int = 0):
+    """ Optimized?
+    """
+    fishes = [int(n) for n in data.strip().split(",")]
+
+    counter = Counter(fishes)
+
+    while days_after:
+        # how many "maturing" ?
+        maturing = counter.pop(0, 0)
+
+        # shift the counters ( 1 -> 0, 2 -> 1, ..., 8 -> 7 )
+        counter = {n - 1: counter[n] for n in range(1, 9)}
+
+        # account for the ones that matured
+        counter[8] = maturing
+        counter[6] += maturing
+
+        days_after -= 1
+
+    return sum(counter.values())
+
+
 test_input = "3,4,3,1,2"
 
 
@@ -28,6 +53,12 @@ def test_part1():
     assert 5934 == part1(test_input, days_after=80)
 
 
+def test_part2():
+    assert 26 == part2(test_input, days_after=18)
+    assert 5934 == part2(test_input, days_after=80)
+
+
 if __name__ == '__main__':
     data = aoc.read_input(__file__)
     print("Part1", part1(data, days_after=80))
+    print("Part2", part2(data, days_after=256))
