73d22674445dcb502cf1ad061781b4eb9ce2ae6e
Gabriel Reis <gabrielcnr@gmail.com>
2021-12-09 14:10:11 +0000

    Day 8 - Part 1

---
+++ day08.py
@@ -0,0 +1,39 @@
+import aoc
+
+test_input = """
+be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"""
+
+
+def part1(data):
+    data = data.strip()
+    inputs = []
+    outputs = []
+    for line in data.strip().splitlines():
+        i, o = line.strip().split(" | ")
+        inputs.append(i)
+        outputs.append(o)
+
+    total = 0
+    for out in outputs:
+        for segs in out.split():
+            if len(segs) in {2, 3, 4, 7}:
+                total += 1
+    return total
+
+
+def test_part1():
+    assert 26 == part1(test_input)
+
+
+if __name__ == '__main__':
+    data = aoc.read_input(__file__)
+    print("Part 1", part1(data))
